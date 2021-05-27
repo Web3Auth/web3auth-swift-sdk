b96a2621f312c60697616556e2e55c41a35ec95b
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-05-27 11:13:27 +0700

    Add top-level Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "OpenLogin",
+    products: [
+        .library(
+            name: "OpenLogin",
+            targets: ["OpenLogin"]),
+    ],
+    dependencies: [
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "OpenLogin",
+            dependencies: [],
+            path: "OpenLogin/Sources/OpenLogin",),
+    ]
+)
