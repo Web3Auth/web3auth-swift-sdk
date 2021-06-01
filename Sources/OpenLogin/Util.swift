87500bc9387552dd0196a0e617ad3681e198416e
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-06-01 16:26:30 +0700

    Add API

---
+++ Sources/OpenLogin/Util.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+func plistValues(_ bundle: Bundle) -> (clientId: String, network: Network)? {
+    guard
+        let path = bundle.path(forResource: "OpenLogin", ofType: "plist"),
+        let values = NSDictionary(contentsOfFile: path) as? [String: Any]
+    else {
+        print("Missing OpenLogin.plist file in your bundle!")
+        return nil
+    }
+    
+    guard
+        let clientId = values["ClientId"] as? String,
+        let network = values["Network"] as? String,
+        let network = Network(rawValue: network)
+    else {
+        print("OpenLogin.plist file at \(path) is missing or having incorrect 'ClientId' and/or 'Network' entries!")
+        print("File currently has the following entries: \(values)")
+        return nil
+    }
+    return (clientId: clientId, network: network)
+}
+
+func decodedBase64(_ base64URLSafe: String) -> Data? {
+    var base64 = base64URLSafe
+        .replacingOccurrences(of: "-", with: "+")
+        .replacingOccurrences(of: "_", with: "/")
+    if base64.count % 4 != 0 {
+        base64.append(String(repeating: "=", count: 4 - base64.count % 4))
+    }
+    return Data(base64Encoded: base64)
+}
---
+++ Sources/OpenLogin/WebAuth.swift
@@ -0,0 +1,107 @@
+import UIKit
+import AuthenticationServices
+
+/**
+ WebAuth Authentication using OpenLogin.
+ */
+@available(iOS 12.0, *)
+public class WebAuth: NSObject {
+    static let sdkURL = URL(string: "https://sdk.openlogin.com")!
+    
+    private let clientId: String
+    private let network: Network
+    
+    public init(clientId: String, network: Network) {
+        self.clientId = clientId
+        self.network = network
+    }
+    
+    /**
+     Starts the WebAuth flow by modally presenting a ViewController in the top-most controller.
+
+     ```
+     OpenLogin
+         .webAuth()
+         .start {
+             switch $0 {
+             case .success(let result):
+                 print("""
+                     Signed in successfully!
+                         Private key: \(result.privKey)
+                         User info:
+                             Name: \(result.userInfo.name)
+                             Profile image: \(result.userInfo.profileImage ?? "N/A")
+                             Type of login: \(result.userInfo.typeOfLogin)
+                     """)
+             case .failure(let error):
+                 print("Error: \(error)")
+             }
+         }
+     ```
+
+     Any on going WebAuth auth session will be automatically cancelled when starting a new one,
+     and it's corresponding callback with be called with a failure result of `WebAuthError.appCancelled`
+
+     - parameter callback: Callback called with the result of the WebAuth flow.
+     */
+    public func start(_ callback: @escaping (Result<State>) -> Void) {
+        guard
+            let bundleId = Bundle.main.bundleIdentifier,
+            let redirectURL = URL(string: "\(bundleId)://openlogin")
+        else { return callback(.failure(WebAuthError.noBundleIdentifierFound)) }
+        
+        let params: [String: Any] = [
+            "init": [
+                "clientId": clientId,
+                "network": network.rawValue,
+                "redirectUrl": redirectURL.absoluteString
+            ]
+        ]
+        
+        guard
+            let data = try? JSONSerialization.data(withJSONObject: params),
+            var components = URLComponents(string: WebAuth.sdkURL.absoluteString)
+        else { return callback(.failure(WebAuthError.unknownError)) }
+        
+        components.path = "/login"
+        components.fragment = data.base64EncodedString()
+        
+        guard let url = components.url
+        else { return callback(.failure(WebAuthError.unknownError)) }
+        
+        let authSession = ASWebAuthenticationSession(
+            url: url, callbackURLScheme: redirectURL.scheme) { callbackURL, authError in
+            guard
+                authError == nil,
+                let callbackURL = callbackURL,
+                let callbackFragment = callbackURL.fragment,
+                let callbackData = decodedBase64(callbackFragment),
+                let callbackState = try? JSONDecoder().decode(State.self, from: callbackData)
+            else {
+                let authError = authError ?? WebAuthError.unknownError
+                if case ASWebAuthenticationSessionError.canceledLogin = authError {
+                    return callback(.failure(WebAuthError.userCancelled))
+                } else {
+                    return callback(.failure(authError))
+                }
+            }
+            callback(.success(callbackState))
+        }
+        
+        if #available(iOS 13.0, *) {
+            authSession.presentationContextProvider = self
+        }
+        
+        if !authSession.start() {
+            callback(.failure(WebAuthError.unknownError))
+        }
+    }
+}
+
+@available(iOS 12.0, *)
+extension WebAuth: ASWebAuthenticationPresentationContextProviding {
+    public func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        let window = UIApplication.shared.windows.first { $0.isKeyWindow }
+        return window ?? ASPresentationAnchor()
+    }
+}
