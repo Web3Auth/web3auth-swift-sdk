20cf07e7475459b164c1979a82004d19d1c49015
michaellee8 <ckmichael8@gmail.com>
2021-12-09 15:39:20 +0800

    fix: use main thread to execute ui related things

--- Sources/OpenLogin/WebAuth.swift
+++ Sources/OpenLogin/WebAuth.swift
@@ -48,65 +48,66 @@ public class WebAuth: NSObject {
      - parameter callback: Callback called with the result of the WebAuth flow.
      */
     public func start(_ callback: @escaping (Result<State>) -> Void) {
-        guard
-            let bundleId = Bundle.main.bundleIdentifier,
-            let redirectURL = URL(string: "\(bundleId)://openlogin")
-        else { return callback(.failure(WebAuthError.noBundleIdentifierFound)) }
-        
-        // Get the current config of fastLogin, defaults to false
-        let fastLogin = UserDefaults.standard.bool(forKey: configFastLoginKey)
-        
-        // Enable fastLogin for subsequent logins
-        UserDefaults.standard.set(true, forKey: configFastLoginKey)
-        
-        let params: [String: Any] = [
-            "init": [
-                "clientId": clientId,
-                "network": network.rawValue,
-                "redirectUrl": redirectURL.absoluteString
-            ],
-            "params": [
-                "fastLogin": fastLogin
-
-            ]
-        ]
-                
-        guard
-            let data = try? JSONSerialization.data(withJSONObject: params),
-            var components = URLComponents(string: WebAuth.sdkURL.absoluteString)
-        else { return callback(.failure(WebAuthError.unknownError)) }
-        
-        components.path = "/login"
-        components.fragment = data.base64EncodedString()
-        
-        guard let url = components.url
-        else { return callback(.failure(WebAuthError.unknownError)) }
-        
-        let authSession = ASWebAuthenticationSession(
-            url: url, callbackURLScheme: redirectURL.scheme) { callbackURL, authError in
+        DispatchQueue.main.async { [self] in
             guard
-                authError == nil,
-                let callbackURL = callbackURL,
-                let callbackFragment = callbackURL.fragment,
-                let callbackData = decodedBase64(callbackFragment),
-                let callbackState = try? JSONDecoder().decode(State.self, from: callbackData)
-            else {
-                let authError = authError ?? WebAuthError.unknownError
-                if case ASWebAuthenticationSessionError.canceledLogin = authError {
-                    return callback(.failure(WebAuthError.userCancelled))
-                } else {
-                    return callback(.failure(authError))
+                let bundleId = Bundle.main.bundleIdentifier,
+                let redirectURL = URL(string: "\(bundleId)://openlogin")
+            else { return callback(.failure(WebAuthError.noBundleIdentifierFound)) }
+            
+            // Get the current config of fastLogin, defaults to false
+            let fastLogin = UserDefaults.standard.bool(forKey: configFastLoginKey)
+            
+            // Enable fastLogin for subsequent logins
+            UserDefaults.standard.set(true, forKey: configFastLoginKey)
+            
+            let params: [String: Any] = [
+                "init": [
+                    "clientId": clientId,
+                    "network": network.rawValue,
+                    "redirectUrl": redirectURL.absoluteString
+                ],
+                "params": [
+                    "fastLogin": fastLogin
+                ]
+            ]
+                    
+            guard
+                let data = try? JSONSerialization.data(withJSONObject: params),
+                var components = URLComponents(string: WebAuth.sdkURL.absoluteString)
+            else { return callback(.failure(WebAuthError.unknownError)) }
+            
+            components.path = "/login"
+            components.fragment = data.base64EncodedString()
+            
+            guard let url = components.url
+            else { return callback(.failure(WebAuthError.unknownError)) }
+            
+            let authSession = ASWebAuthenticationSession(
+                url: url, callbackURLScheme: redirectURL.scheme) { callbackURL, authError in
+                guard
+                    authError == nil,
+                    let callbackURL = callbackURL,
+                    let callbackFragment = callbackURL.fragment,
+                    let callbackData = decodedBase64(callbackFragment),
+                    let callbackState = try? JSONDecoder().decode(State.self, from: callbackData)
+                else {
+                    let authError = authError ?? WebAuthError.unknownError
+                    if case ASWebAuthenticationSessionError.canceledLogin = authError {
+                        return callback(.failure(WebAuthError.userCancelled))
+                    } else {
+                        return callback(.failure(authError))
+                    }
                 }
+                callback(.success(callbackState))
             }
-            callback(.success(callbackState))
-        }
-        
-        if #available(iOS 13.0, *) {
-            authSession.presentationContextProvider = self
-        }
-        
-        if !authSession.start() {
-            callback(.failure(WebAuthError.unknownError))
+            
+            if #available(iOS 13.0, *) {
+                authSession.presentationContextProvider = self
+            }
+            
+            if !authSession.start() {
+                callback(.failure(WebAuthError.unknownError))
+            }
         }
     }
     
