bbcd64c8c78482c1bd1bcb086881c1ff45bd8085
michaellee8 <ckmichael8@gmail.com>
2022-03-08 14:43:32 +0800

    chore: whitelabel test

--- OpenloginSwiftSdkDemo/OpenloginSwiftSdkDemo/ContentView.swift
+++ OpenloginSwiftSdkDemo/OpenloginSwiftSdkDemo/ContentView.swift
@@ -60,6 +60,24 @@ struct ContentView: View {
                 }
             )
             
+            Button(
+                action: {
+                    OpenLogin(OLInitParams(clientId: "BJYIrHuzluClBK0vvTBUJ7kQylV_Dj3NA-X1q4Qvxs2Ay3DySkacOpoOb83lDTHJRVY83bFlYtt4p8pQR-oCYtw", network: .testnet, whiteLabel: OLWhiteLabelData(name: "Web3Auth Stub", dark: true, theme: ["primary": "#123456"])))
+                        .login(OLLoginParams(loginProvider: .GOOGLE)) {
+                            switch $0 {
+                            case .success(let result):
+                                showResult(result: result)
+                            case .failure(let error):
+                                print("Error: \(error)")
+                            }
+                        }
+                },
+                label: {
+                    Text("Sign In with Whitelabel")
+                        .padding()
+                }
+            )
+            
  
             
             Text(text).foregroundColor(.white)
