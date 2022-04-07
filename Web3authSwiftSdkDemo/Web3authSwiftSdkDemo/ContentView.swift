e9ace64a978c439a7ba407be92553500ea617b67
michaellee8 <ckmichael8@gmail.com>
2022-04-07 12:27:35 +0800

    docs: add more examples

--- Web3authSwiftSdkDemo/Web3authSwiftSdkDemo/ContentView.swift
+++ Web3authSwiftSdkDemo/Web3authSwiftSdkDemo/ContentView.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 import Web3Auth
 
-
 struct ContentView: View {
     @SwiftUI.State var text = ""
     var body: some View {
@@ -12,7 +11,7 @@ struct ContentView: View {
                         .login(W3ALoginParams()) {
                             switch $0 {
                             case .success(let result):
-                                    showResult(result: result)
+                                showResult(result: result)
                             case .failure(let error):
                                 print("Error: \(error)")
                             }
@@ -62,8 +61,29 @@ struct ContentView: View {
             
             Button(
                 action: {
-                    Web3Auth(W3AInitParams(clientId: "BJYIrHuzluClBK0vvTBUJ7kQylV_Dj3NA-X1q4Qvxs2Ay3DySkacOpoOb83lDTHJRVY83bFlYtt4p8pQR-oCYtw", network: .testnet, whiteLabel: W3AWhiteLabelData(name: "Web3Auth Stub", dark: true, theme: ["primary": "#123456"])))
-                        .login(W3ALoginParams(loginProvider: .GOOGLE)) {
+                    Web3Auth()
+                        .login(W3ALoginParams(
+                            loginProvider: .EMAIL_PASSWORDLESS,
+                            extraLoginOptions: ExtraLoginOptions(
+                                display: nil,
+                                prompt: nil,
+                                max_age: nil,
+                                ui_locales: nil,
+                                id_token_hint: nil,
+                                id_token: nil,
+                                login_hint: "your-email@example.com",
+                                acr_values: nil,
+                                scope: nil,
+                                audience: nil,
+                                connection: nil,
+                                domain: nil,
+                                client_id: nil,
+                                redirect_uri: nil,
+                                leeway: nil,
+                                verifierIdField: nil,
+                                isVerifierIdCaseSensitive: nil
+                            )
+                        )) {
                             switch $0 {
                             case .success(let result):
                                 showResult(result: result)
@@ -73,38 +93,114 @@ struct ContentView: View {
                         }
                 },
                 label: {
+                    Text("Sign In with Email Passwordless")
+                        .padding()
+                }
+            )
+            
+            Button(
+                action: {
+                    Web3Auth(
+                        W3AInitParams(
+                            clientId: "your-client-id",
+                            network: .testnet,
+                            whiteLabel: W3AWhiteLabelData(
+                                name: "Web3Auth Stub",
+                                dark: true,
+                                theme: ["primary": "#123456"]
+                            )
+                        )
+                    )
+                    .login(W3ALoginParams(loginProvider: .GOOGLE)) {
+                        switch $0 {
+                        case .success(let result):
+                            showResult(result: result)
+                        case .failure(let error):
+                            print("Error: \(error)")
+                        }
+                    }
+                },
+                label: {
                     Text("Sign In with Whitelabel")
                         .padding()
                 }
             )
             
- 
+            Button(
+                action: {
+                    Web3Auth(
+                        W3AInitParams(
+                            clientId: "your-client-id",
+                            network: .testnet,
+                            loginConfig: [
+                                "jwt": W3ALoginConfig(
+                                    verifier: "your-verifier-id",
+                                    typeOfLogin: TypeOfLogin.jwt,
+                                    name: "display name",
+                                    verifierSubIdentifier: "sub"
+                                )
+                            ]
+                        )
+                    )
+                    .login(W3ALoginParams(
+                        loginProvider: .GOOGLE,
+                        extraLoginOptions: ExtraLoginOptions(
+                            display: nil,
+                            prompt: nil,
+                            max_age: nil,
+                            ui_locales: nil,
+                            id_token_hint: nil,
+                            id_token: "jwt-id-token",
+                            login_hint: nil,
+                            acr_values: nil,
+                            scope: nil,
+                            audience: nil,
+                            connection: nil,
+                            domain: nil,
+                            client_id: nil,
+                            redirect_uri: nil,
+                            leeway: nil,
+                            verifierIdField: nil,
+                            isVerifierIdCaseSensitive: nil
+                        )
+                    )) {
+                        switch $0 {
+                        case .success(let result):
+                            showResult(result: result)
+                        case .failure(let error):
+                            print("Error: \(error)")
+                        }
+                    }
+                },
+                label: {
+                    Text("Sign In with Custom Authentication")
+                        .padding()
+                }
+            )
             
             Text(text).foregroundColor(.white)
-            
         }
-        
     }
     
-    func showResult(result: Web3AuthState){
+    func showResult(result: Web3AuthState) {
         print("""
-            Signed in successfully!
-                Private key: \(result.privKey)
-                Ed25519 Private key: \(result.ed25519PrivKey)
-                User info:
-                    Name: \(result.userInfo.name)
-                    Profile image: \(result.userInfo.profileImage ?? "N/A")
-                    Type of login: \(result.userInfo.typeOfLogin)
-            """)
+        Signed in successfully!
+            Private key: \(result.privKey)
+            Ed25519 Private key: \(result.ed25519PrivKey)
+            User info:
+                Name: \(result.userInfo.name)
+                Profile image: \(result.userInfo.profileImage ?? "N/A")
+                Type of login: \(result.userInfo.typeOfLogin)
+        """)
         text = """
-            Signed in successfully!
-                Private key: \(result.privKey)
-                Ed25519 Private key: \(result.ed25519PrivKey)
-                User info:
-                    Name: \(result.userInfo.name)
-                    Profile image: \(result.userInfo.profileImage ?? "N/A")
-                    Type of login: \(result.userInfo.typeOfLogin)
-            """
+        Signed in successfully!
+            Private key: \(result.privKey)
+            Ed25519 Private key: \(result.ed25519PrivKey)
+            User info:
+                Name: \(result.userInfo.name)
+                Profile image: \(result.userInfo.profileImage ?? "N/A")
+                Type of login: \(result.userInfo.typeOfLogin)
+        """
     }
 }
 
