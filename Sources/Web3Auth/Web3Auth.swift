e463f9c5b77b5b2ca0a338f335e8831acc57258a
Gaurav Goel <gaurav@tor.us>
2024-04-04 09:29:12 +0530

    updated decodeStateFromCallbackURL function.

--- Sources/Web3Auth/Web3Auth.swift
+++ Sources/Web3Auth/Web3Auth.swift
@@ -377,11 +377,13 @@ public class Web3Auth: NSObject {
     }
 
     static func decodeStateFromCallbackURL(_ callbackURL: URL) throws -> SessionResponse {
-       guard
-            let callbackFragment = callbackURL.fragment,
-            let callbackData = Data.fromBase64URL(callbackFragment.components(separatedBy: "b64Params=")[1]),
+        guard
+            let components = URLComponents(url: callbackURL, resolvingAgainstBaseURL: false),
+            let queryItems = components.queryItems,
+            let b64ParamsItem = queryItems.first(where: { $0.name == "b64Params" }),
+            let callbackFragment = b64ParamsItem.value,
+            let callbackData = Data.fromBase64URL(callbackFragment),
             let callbackState = try? JSONDecoder().decode(SessionResponse.self, from: callbackData)
-
         else {
             throw Web3AuthError.decodingError
         }
